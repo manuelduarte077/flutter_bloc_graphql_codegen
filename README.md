19b59ace84090c248daa2340e48753f87a36020b
manuelduarte077 <carlosd99m@gmail.com>
2022-10-10 22:24:17 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SpaceX
+
+# Packages
+
+# Demo
+
---
+++ android/app/src/main/kotlin/com/manuelduarte077/flutter_bloc_graphql/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manuelduarte077.flutter_bloc_graphql
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
